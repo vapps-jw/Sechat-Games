75bef38e16f2c8d28772b0ac8abedc806b5521cd
JW <jonasz.witek@pm.me>
2024-03-29 21:14:27 +0100

    updates

--- src/pages/GamesIndex.jsx
+++ src/pages/GamesIndex.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useStore } from "../App";
 import { createNewClient, SignalRClient } from "../SignalRClient";
 
